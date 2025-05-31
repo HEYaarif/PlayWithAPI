6ec624c6b7b8b25e21304b34bf3f9169a3732d4e
HEYaarif <aarif.mdaarif2002@gmail.com>
2025-06-01 03:13:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-pink-800 text-white text-center font-bold p-4 text-3xl font-serif md:bg-blue-500 md:text-red-600'>
+      <h2>Hello sir</h2>
+    </div>
+  )
+}
+
+export default App
